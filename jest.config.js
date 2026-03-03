2ecff26aaa7b7a737b77763cc20b3c6857acdd35
Sebastian Heide-Meyer zu Erpen <sebastianhmze@gmail.com>
2026-03-03 14:16:16 +0100

    fix(jest): switch to @swc/jest transformer to handle pdfjs-dist v5 ESM (import.meta)

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-  //    preset: 'ts-jest',
-  //    testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts|tsx|js|jsx|mjs)$': ['babel-jest', { configFile: './babel.config.js' }],
-    '^.+\\.tsx?$': ['@swc/jest'],
+    '^.+\.(ts|tsx|js|jsx|mjs)$': ['@swc/jest'],
   },
   transformIgnorePatterns: ['/node_modules/(?!(pdfjs-dist)/)'],
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
-//  transform: {
-//    '^.+\\.tsx?$': ['@swc/jest'],
-//  },
 };
