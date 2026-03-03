f333c18374302a10309daa51a8a4cfcb10b40cba
Sebastian Heide-Meyer zu Erpen <sebastianhmze@gmail.com>
2026-03-03 18:28:56 +0100

    fix(jest): force @swc/jest to emit CommonJS to prevent __dirname shim conflict with pdfjs-dist v5

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   transform: {
-    '^.+\.(ts|tsx|js|jsx|mjs)$': ['@swc/jest'],
+    '^.+\.(ts|tsx|js|jsx|mjs)$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript' },
+          target: 'es2020',
+        },
+        module: {
+          type: 'commonjs',
+        },
+      },
+    ],
   },
   transformIgnorePatterns: ['/node_modules/(?!(pdfjs-dist)/)'],
   testEnvironment: 'node',
